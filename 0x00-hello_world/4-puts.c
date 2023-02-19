00c469346d246fe4a19379f67f79f05ac181a2c9
janechidimma <chukwuchidimma25@gmail.com>
2023-02-18 21:43:20 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
-#include stdio.h
+#include <stdio.h>
+/**
+ *main - A program that prints exactly a string and add a new line
+* Return: 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
