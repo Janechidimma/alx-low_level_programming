b5754e58870f1580c2e856f1519cf009207d26ad
janechidimma <chukwuchidimma25@gmail.com>
2023-02-20 03:30:46 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: Always 0
+  */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	while (m <= 90)
+	{
+		putchar(n);
+		n++;
+	}
+	{
+		putchar(m);
+		m++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
