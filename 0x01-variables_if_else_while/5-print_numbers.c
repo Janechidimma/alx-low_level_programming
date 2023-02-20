e140626b9311d2bcd0a9bf1033c1a821444b5c9f
janechidimma <chukwuchidimma25@gmail.com>
2023-02-20 10:15:15 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,7 +8,8 @@ int main(void)
 	int number;
 for (number = 1; number <= 9; number++)
 {
-printf("%d\n", number);
+printf("%d", number);
 }
+printf("\n");
 return (0);
 }
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+  *main - print single digit nunber of base 10
+  *Return: Always 0
+  */
+int main(void)
+{
+int
