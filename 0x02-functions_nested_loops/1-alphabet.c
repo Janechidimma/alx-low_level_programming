ac7c06164d5fbb8de302a1a4fec71993408bd33e
janechidimma <chukwuchidimma25@gmail.com>
2023-02-21 21:00:46 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *main - check the code
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		_putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
