43c505529674bce49a8143668a89036b282ee2fc
janechidimma <chukwuchidimma25@gmail.com>
2023-02-22 21:01:32 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_abs- computes absolute value of integer
+  *@m: character to be checked
+  *Return: always return 0
+  */
+int _abs(int m)
+{
+	if (m >= 0)
+	{
+		return (m);
+	}
+	else
+	{
+		return (m * -1);
+	}
+}
