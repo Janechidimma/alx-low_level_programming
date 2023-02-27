4118419dfb12b2f2f2a06527859131181cd9f082
janechidimma <chukwuchidimma25@gmail.com>
2023-02-27 06:33:10 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,3 +1,14 @@
 #include "main"
 /**
-  * 
+  *_strlen - length of a string
+  * @s: string of characters
+  *Return: Always return 0
+  */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+	return (count);
+}
