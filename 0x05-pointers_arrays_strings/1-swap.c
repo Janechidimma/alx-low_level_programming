717d1f0d0ab04538481f151534afbbc23b0c7319
janechidimma <chukwuchidimma25@gmail.com>
2023-02-27 06:25:15 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+  *swap_int - swap between two numbers
+  *@a: character to be swapped
+  *@b: character to be swapped
+  * return: Aleays return void
+  */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
