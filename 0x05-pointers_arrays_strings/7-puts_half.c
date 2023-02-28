a6de10959beb0c4f21d8fb7e391dc0f9a12948c3
janechidimma <chukwuchidimma25@gmail.com>
2023-02-27 18:32:23 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,17 +6,15 @@
   */
 void puts_half(char *str)
 {
-	int count = 0, i;
+	int a, n, longi;
 
-	while (*str)
-	{
-		count++;
-		str++;
-	}
-	for (i = 0; i < count; i++)
-		str--;
-	i = (count % 2 == 0) ? count / 2 (count + 1) / 2
-		for (; i < count; i++)
-			_putchar(str[i];
-					_putchar('\n);
+	longi = 0;
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+	n = (longi / 2);
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
 }
