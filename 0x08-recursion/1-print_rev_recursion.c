eae8a5113842ee5fec8661c0e34879991cfb4a0e
janechidimma <chukwuchidimma25@gmail.com>
2023-03-09 20:13:15 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_print_rev_recursion- print a string in reverse
+  *@s: pointer to a string
+  *retirn: return 0 always
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
